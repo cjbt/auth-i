be66d1f36c5da77df3f074b119f5c2f192dd5191
cecil john tantay <cjbtantay@gmail.com>
2019-02-25 14:04:37 -0800

    postgres@

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 const knexConfig = require('../knexfile');
 
 const environment = process.env.ENVIRONMENT || 'development';
-const db = knex(knexConfig[environment]);
+const db = knex(knexConfig['production']);
 
 module.exports = db;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const db = require('./data/dbConfig');
 
 const server = express();
 server.use(express.json());
@@ -13,6 +14,16 @@ server.use(cors());
 server.get('/', (req, res) => {
   res.send('<h1>Server Is Working</h1>');
 });
+
+server.get('/users', (req, res) => {
+  db('auth')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post('/register', (req, res) => {});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
