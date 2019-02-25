0d5574a892202edac0c52a0f35507e5a2b53c723
cecil john tantay <cjbtantay@gmail.com>
2019-02-25 14:21:15 -0800

    hello

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 const knexConfig = require('../knexfile');
 
 const environment = process.env.ENVIRONMENT || 'development';
-const db = knex(knexConfig['production']);
+const db = knex(knexConfig[environment]);
 
 module.exports = db;
