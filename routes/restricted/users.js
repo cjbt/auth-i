fcec23118fb1aba5fd532bac044ebaa1220cc7d1
cecil john tantay <cjbtantay@gmail.com>
2019-02-26 14:01:35 -0800

    add cookie session mvp

--- routes/restricted/users.js
+++ routes/restricted/users.js
@@ -12,4 +12,17 @@ route.get('/users', authenticate.auth, (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+route.get('/logout', authenticate.auth, (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ message: 'You cannot log out' });
+      } else {
+        res.json({ message: 'You logged out succesfully' });
+      }
+    });
+  } else {
+    res.end();
+  }
+});
 module.exports = route;
